d22610239fedb27d4b2bfd285464a528f79a401d
dbutterw <david.butterwick@gmail.com>
2019-09-12 16:17:02 -0400

    react/redux testing play

--- react-ts-webpack/src/app/components/Child.tsx
+++ react-ts-webpack/src/app/components/Child.tsx
@@ -1,10 +1,46 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 
-export default class Child extends React.Component<any> {
+interface DispatchProps {
+    punt:(id:string)=>void;
+}
+
+interface StateProps {
+    result:string;
+}
+
+interface OwnProps {
+  x?:string;
+}
+
+class Child extends React.Component<DispatchProps&StateProps&OwnProps> {
     constructor(props:any){
         super(props);
     }
+
+    onClick=(ev: React.MouseEvent<HTMLDivElement>)=> {
+        ev.preventDefault();
+        this.props.punt('kk');
+    }
     render() {
-        return (<div>Hello Child{this.props.x}</div>)
+        return (<div id='dave' onClick={this.onClick}>Hello Child{this.props.x}</div>)
     }
 }
+
+function mapStateToProps(state: any): StateProps {
+    return {
+        result:state.sampleReducer
+    }
+}
+
+
+function mapDispatchToProps(dispatch: Dispatch): DispatchProps {
+    return {
+        punt: (id:string) => {
+            dispatch({type: 'X', id});
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Child);
