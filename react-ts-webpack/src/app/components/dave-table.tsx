0d0ccce82251dc7e17401987e7c12169c62f504b
D Butterwick <david.butterwick@gmail.com>
2020-01-30 22:06:48 -0500

    .

--- react-ts-webpack/src/app/components/dave-table.tsx
+++ react-ts-webpack/src/app/components/dave-table.tsx
@@ -1,4 +1,4 @@
-import { GridOptions } from 'ag-grid-community';
+import { GridOptions, GridReadyEvent, RowDataChangedEvent } from 'ag-grid-community';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import { AgGridReact } from 'ag-grid-react';
 import { AgGridReactProps } from 'ag-grid-react/lib/agGridReact';
@@ -15,15 +15,29 @@ export class DaveTable extends React.Component<Partial<AgGridReactProps>& OwnPro
     super(props);
   }
 
+  onGridReady=(ev:GridReadyEvent)=> {
+    const { onGridReady } = this.props;
+    ev.api.sizeColumnsToFit();
+    onGridReady && onGridReady(ev);
+  };
+
+  onRowDataChanged=(ev:RowDataChangedEvent)=> {
+    const { onRowDataChanged } = this.props;
+    ev.api.sizeColumnsToFit();
+    onRowDataChanged && onRowDataChanged(ev);
+  };
+
   render() {
     const { height, label, borderColor, ...rest } = this.props;
     const layout = height != null ? 'normal' : 'autoHeight';
     const bc = borderColor ? { border: `2px solid ${borderColor}`   }:{};
     const style = { height, width:'100%', ...bc };
     return (
-        <div id={label} className="ag-theme-balham" style={ {...style} }>
+        <div id={label} className="ag-theme-balham" style={ { ...style } }>
           <AgGridReact
             domLayout={layout}
+            onGridReady={this.onGridReady}
+            onRowDataChanged={this.onGridReady}
             {...rest}
             />
         </div>
